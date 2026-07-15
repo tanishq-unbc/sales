67d36e1dce62e1d06f611ca631df2e911e075c56
Tanishq Lende <tanishq.lende@gmail.com>
2026-07-15 16:31:52 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { db, logAudit } = require('./db/database');
+const { db, logAudit } = require('./database');
 const { matchLpoToQuotation, checkCustomerLedger } = require('./logic');
 
 const app = express();
